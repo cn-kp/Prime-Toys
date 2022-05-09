eb27c750cd47bd29d56f52895dcd91284f551c62
kevin peng <peng123kevin@gmail.com>
2022-05-09 22:02:21 +1000

    started schemas

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,36 @@
+const { gql } = require("apollo-server-express");
+
+const typeDefs = gql `
+    type Categories {
+        _id: id
+        name: String
+    }
+    type Toys {
+        _id: ID
+        name: String
+        description: String
+        image: String
+    }
+    type User {
+        _id: id
+        username: String
+        email: String
+        listing: [Toys]
+    }
+    type Auth {
+        token: ID
+        user: User
+    }
+    type Query {
+        categories: [Categories]
+        toys(categories: ID, name: String): [Toys]
+        user: User
+    }
+    type Mutation {
+        addUser(username: String!, email: String!, password: String!): Auth
+        updateUser(username: String, email: String, password: String): User
+        login(email: String!, password: String!): Auth
+    }
+`;
+
+module.exports = typeDefs
