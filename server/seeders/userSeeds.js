757ceb9dc654d54f8f0604c18e5ce0579a5f6832
Jonathan Florido-Carait <jonathan.carait@outlook.com>
2022-05-10 15:45:20 +1000

    setup seed data in seeders folder

---
+++ server/seeders/userSeeds.js
@@ -0,0 +1,23 @@
+[
+  {
+    username: 'batman',
+    email: 'batman@testmail.com',
+    password: 'password12345',
+    listings: {},
+  },
+  {
+    username: 'robin',
+    email: 'robin@testmail.com',
+    password: 'password12345',
+  },
+  {
+    username: 'jono',
+    email: 'jono@testmail.com',
+    password: 'password12345',
+  },
+  {
+    username: 'kevin',
+    email: 'kevin@testmail.com',
+    password: 'password12345',
+  },
+];
