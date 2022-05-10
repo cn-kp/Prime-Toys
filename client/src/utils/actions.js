a9310a85219cec7a201311328e3bbc7376bf2fe0
kevin peng <peng123kevin@gmail.com>
2022-05-10 20:27:20 +1000

    utils/queries

---
+++ client/src/utils/actions.js
@@ -0,0 +1,1 @@
+export const UPDATE_TOYS = "UPDATE_TOYS"
---
+++ client/src/utils/queries.js
@@ -0,0 +1,56 @@
+import {gql} from '@apollo/client'
+
+export const QUERY_ALL_TOYS = gql`
+    {
+        toy {
+            _id
+            name
+            description
+            image
+            category {
+                name
+            }
+        }
+    }
+`
+export const QUERY_USER = gql`
+    {
+        user {
+            _id
+            username
+            email
+            listings{
+                _id
+                name
+                description
+                image
+                category {
+                    name
+                }
+            }
+        }
+    }
+`
+
+export const QUERY_CATEGORY = gql`
+    {
+        categories{
+            _id
+            name
+        }
+    }
+`
+
+export const QUERY_USER_TOYS = gql`
+    query getUserToys($user: ID){
+        toy(user: $user){
+            _id
+            name
+            description
+            image
+            category {
+                name
+            }
+        }
+    }
+`
