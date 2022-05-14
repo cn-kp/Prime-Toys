e3c16d07e201ee8710c162182213703697b4a985
Jonathan Florido-Carait <jonathan.carait@outlook.com>
2022-05-14 21:11:39 +1000

    working on card elements

--- client/src/components/ToyCard/index.js
+++ client/src/components/ToyCard/index.js
@@ -1,25 +1,25 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React from 'react';
+import { Link } from 'react-router-dom';
 
-import "./ToyCard.scss";
+import './ToyCard.scss';
 
 export default function ToyCard(card) {
   const { _id, name, description, image, category } = card;
 
   return (
-      <div className="card">
-        <div className="image--container">
-          <img alt={name} src={`/images/${image}`} />
+    <div className="card">
+      <div className="image--container">
+        <img alt={name} src={`/images/${image}`} />
+      </div>
+      <div className="card--body">
+        <div className="card--text">
+          <p className="toy--name">{name}</p>
+          <p className="toy--description">{category}</p>
         </div>
-        <div className="card--body">
-          <div className="card--text">
-          <p className="toy-name">{name}</p>
-          <p>{category}</p>
-          </div>
-          <div className="card--link">
+        <div className="card--link">
           <button className="contact--seller">Email seller</button>
-          </div>
         </div>
       </div>
+    </div>
   );
 }
