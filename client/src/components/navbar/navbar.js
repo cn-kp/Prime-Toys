b2f64bd4b2f6ee81cdd9c355406313b83f0e7772
Jonathan Florido-Carait <jonathan.carait@outlook.com>
2022-05-15 03:54:38 +1000

    added mobile menu

--- client/src/components/navbar/navbar.js
+++ client/src/components/navbar/navbar.js
@@ -1,23 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './navbar.scss';
 
 export default function Navbar() {
+  const [navbarOpen, setNavbarOpen] = useState(false);
+
+  const handleToggle = () => {
+    setNavbarOpen((prev) => !prev);
+  };
+
+  const closeMenu = () => {
+    setNavbarOpen(false);
+  };
+
   return (
     <nav>
       <div className="navbar">
         <div className="title-container">
           <h2 className="header-title">Prime Toys</h2>
         </div>
-        <ul className="nav--items">
+        <button className="btn" onClick={handleToggle}>
+          {navbarOpen ? (
+            <i class="fa-solid fa-xmark"></i>
+          ) : (
+            <i class="fa-solid fa-bars"></i>
+          )}
+        </button>
+        <ul className={`nav-items ${navbarOpen ? ' showMenu' : ''}`}>
           <li>
-            <Link to="/">Home</Link>
+            <Link to="/" activeClassName="active-link" onClick={closeMenu}>
+              Home
+            </Link>
           </li>
           <li>
-            <Link to="/profile">Profile</Link>
+            <Link
+              to="/profile"
+              activeClassName="active-link"
+              onClick={closeMenu}
+            >
+              Profile
+            </Link>
           </li>
           <li>
-            <Link to="/listings">Listings</Link>
+            <Link
+              to="/listings"
+              activeClassName="active-link"
+              onClick={closeMenu}
+            >
+              Listings
+            </Link>
           </li>
           <li>
             <Link to="/login">Login</Link>
