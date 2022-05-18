41d0c6e42dcdf6c5c2f8efa27d0d850f7b6a838c
kevin peng <peng123kevin@gmail.com>
2022-05-18 17:27:25 +1000

    added alert feature to indicate new adds and removes and fixed up issue where listing was not ading properly when select tags were not selected

--- client/src/components/ToyCard/index.js
+++ client/src/components/ToyCard/index.js
@@ -2,12 +2,18 @@ import React, { useState } from "react";
 import { useMatch } from "react-router-dom";
 import { useMutation } from "@apollo/client";
 import { REMOVE_TOY } from "../../utils/mutations";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 import "./ToyCard.scss";
 
 export default function ToyCard(card) {
   const { id, name, description, image, category, isFree } = card;
   const [RemoveToy] = useMutation(REMOVE_TOY);
+
+  const removeNotify = () => {
+    toast("listing removed successfully");
+  };
 
   const removeToyHandler = async (event) => {
     event.preventDefault();
@@ -18,6 +24,7 @@ export default function ToyCard(card) {
           id: value,
         },
       });
+      removeNotify();
     } catch (err) {
       console.log(err);
     }
